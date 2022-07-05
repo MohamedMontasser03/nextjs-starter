e9b822c4e6e6ae6279407b983a4d2118c546a360
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-07-05 14:46:02 +0200

    fixed build

--- src/layouts/main-page/MainPage.tsx
+++ src/layouts/main-page/MainPage.tsx
@@ -1,12 +1,3 @@
-import type { NextPage } from "next";
 import Home from "../../components/home/Home";
 
-const MainPage: NextPage = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-};
-
-export default MainPage;
+export default Home;
